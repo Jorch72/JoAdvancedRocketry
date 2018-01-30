e4373a2364f12ae44098046620ed7704f38022af
Jo Jaquinta <jo@111george.com>
2018-01-30 13:50:25 -0500

    Path validation.

--- jo.ar.gen/src/jo/ar/gen/cmd/GenerateSystems.java
+++ jo.ar.gen/src/jo/ar/gen/cmd/GenerateSystems.java
@@ -6,6 +6,7 @@ public class GenerateSystems
 {
     private String[] mArgs;
     private File     mConfigDir;
+    private File     mGalaxyFile;
     
     public GenerateSystems(String[] args)
     {
@@ -15,6 +16,37 @@ public class GenerateSystems
     public void run()
     {
         parseArgs();
+        setup();
+    }
+    
+    private void setup()
+    {
+        if (mConfigDir == null)
+        {
+            System.err.println("Need to set configuration dir with --dir parameter");
+            System.exit(1);
+        }
+        if (!mConfigDir.exists())
+        {
+            System.err.println("Path '"+mConfigDir+"' does not exist.");
+            System.exit(1);
+        }
+        if (!mConfigDir.isDirectory())
+        {
+            System.err.println("Path '"+mConfigDir+"' is not a directory.");
+            System.exit(1);
+        }
+        mGalaxyFile = new File(mConfigDir, "planetDefs.xml");
+        if (!mGalaxyFile.exists())
+        {
+            System.err.println("Path '"+mGalaxyFile+"' does not exist.");
+            System.exit(1);
+        }
+        if (!mGalaxyFile.isFile())
+        {
+            System.err.println("Path '"+mGalaxyFile+"' is not a file.");
+            System.exit(1);
+        }
     }
     
     private void parseArgs()
